a3163f3033a105404e51798422de0112d8b04d72
fogxl <yumin6@ualberta.ca>
2025-10-28 22:28:01 -0600

    edited Event class

--- app/src/main/java/com/example/dangle_lotto/Event.java
+++ app/src/main/java/com/example/dangle_lotto/Event.java
@@ -8,4 +8,64 @@ public class Event {
     DateTime time;
     String location;
     String description;
+    int eventSize;
+
+    public Event(String name, DateTime date, DateTime time, String location, String description, int eventSize) {
+        this.name = name;
+        this.date = date;
+        this.time = time;
+        this.location = location;
+        this.description = description;
+        this.eventSize = eventSize;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public DateTime getTime() {
+        return time;
+    }
+
+    public void setTime(DateTime time) {
+        this.time = time;
+    }
+
+    public DateTime getDate() {
+        return date;
+    }
+
+    public void setDate(DateTime date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEventSize () {
+        return eventSize;
+    }
+
+    public void setEventSize(int eventSize) {
+        this.eventSize = eventSize;
+    }
+
+
 }

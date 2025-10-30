0ecce30076c6eefe744052bd2b7bcdfb7db52d7e
Annie D <ading2@ualberta.ca>
2025-10-30 14:09:00 -0600

    Added events to db

--- app/src/main/java/com/example/dangle_lotto/FirebaseManager.java
+++ app/src/main/java/com/example/dangle_lotto/FirebaseManager.java
@@ -3,12 +3,17 @@ package com.example.dangle_lotto;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.SimpleDateFormat;
 import java.util.Map;
+import java.util.Calendar;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class FirebaseManager {
     private FirebaseFirestore db;
     private CollectionReference users;
     private CollectionReference events;
+
     FirebaseManager() {
         db = FirebaseFirestore.getInstance();
         users = db.collection("users");
@@ -19,16 +24,28 @@ public class FirebaseManager {
         return db;
     }
 
-    public void addUser(String uid, String first_name, String last_name, String email){
+    public void addUser(String uid, String first_name, String last_name, String email) {
         Map<String, String> data = Map.of(
                 "First Name", first_name,
                 "Last Name", last_name,
                 "Email", email
         );
 
-        users.document(uid).set(data);;
+        users.document(uid).set(data);
 
 
+
+    }
+
+    public void addEvent(String event_id, String title, String description, int limit, String deadline) {
+        Map<String, String> data = Map.of(
+                "Title", title,
+                "Description", description,
+                "Limit", Integer.toString(limit),
+                "Deadline", deadline
+        );
+
+        events.document(event_id).set(data);
     }
 
 }
--- app/src/main/java/com/example/dangle_lotto/ui/Event.java
+++ app/src/main/java/com/example/dangle_lotto/ui/Event.java
@@ -1,4 +1,17 @@
 package com.example.dangle_lotto.ui;
 
+import com.example.dangle_lotto.User;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Event {
+    private String title;
+    private String description;
+    private int limit;
+    private Date deadline;
+
+    private ArrayList<User> entrants;
+    private ArrayList<User> chosen;
 }

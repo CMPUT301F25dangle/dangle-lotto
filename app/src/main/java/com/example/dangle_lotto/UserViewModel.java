4c28e4e097239bfd750a7e39ceeaadc5d08f550c
amongus <adi4soni@gmail.com>
2025-11-05 13:25:03 -0700

    initial userviewmodel idea

---
+++ app/src/main/java/com/example/dangle_lotto/UserViewModel.java
@@ -0,0 +1,97 @@
+package com.example.dangle_lotto;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+
+/**
+ * Class will contain all user information that needs to exist between fragments
+ * <p>
+ * Contains methods to get user information and update it.
+ *
+ * @author Aditya Soni
+ * @version 1.0
+ * @since 2025-11-05
+ */
+public class UserViewModel extends ViewModel {
+    private final FirebaseManager firebaseManager = new FirebaseManager();
+    private final MutableLiveData<User> user = new MutableLiveData<>();
+    private final MutableLiveData<ArrayList<Event>> yourEvents = new MutableLiveData<>();
+    private boolean yourEventsLoaded = false;
+    private final MutableLiveData<ArrayList<Event>> organizedEvents = new MutableLiveData<>();;
+    private boolean organizedEventsLoaded = false;
+
+    /**
+     * Sets the user object
+     *
+     * @param user user object
+     */
+    public void setUser(User user) {
+        this.user.setValue(user);
+    }
+
+
+    /**
+     * Gets the user object that is currently in the View Model
+     *
+     * @return user object
+     */
+    public MutableLiveData<User> getUser() {
+        return user;
+    }
+
+    /**
+     * Gets yourEvents that are currently in the View Model
+     *
+     * @return Arraylist of events that the user has signed up for in some way
+     */
+    public MutableLiveData<ArrayList<Event>> getYourEvents() {
+        return yourEvents;
+    }
+
+    /**
+     * Fetches users yourEvents data from firebase if it hasn't already been fetched.
+     * <p>
+     * Sets boolean to false, so that this can only occur once in an instance of the app running.
+     */
+    public void loadYourEvents() {
+        if (!yourEventsLoaded) {
+            // fetch yourEvents from firebase and if successful then use
+            // yourEvents.postValue(some data) to update yourEvents in the View Model
+            // and set yourEventsLoaded to true
+        }
+    }
+
+    public void addYourEvent(Event event) {
+        ArrayList<Event> currentList = getYourEvents().getValue();
+        if (currentList == null) {
+            currentList = new ArrayList<>();
+        }
+        currentList.add(event);
+        getYourEvents().setValue(currentList);
+    }
+
+    /**
+     * Gets organizedEvents that are currently in the View Model
+     *
+     * @return Arraylist of events that the user has organized
+     */
+    public MutableLiveData<ArrayList<Event>> getOrganizedEvents() {
+        return organizedEvents;
+    }
+
+    /**
+     * Fetches users organizedEvents data from firebase if it hasn't already been fetched.
+     * <p>
+     * Sets boolean to false, so that this can only occur once in an instance of the app running.
+     */
+    public void loadOrganizedEvents() {
+        if (!organizedEventsLoaded) {
+            // fetch organizedEvents from firebase and if successful then use
+            // organizedEvents.postValue(some data) to update organizedEvents in the View Model
+            // and set organizedEventsLoaded to true
+        }
+    }
+
+}

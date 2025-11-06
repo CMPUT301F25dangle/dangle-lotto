d4773304d110e13d918991c8c33b7abd9df8f7b0
Annie D <ading2@ualberta.ca>
2025-11-06 00:17:14 -0700

    Some stuff

--- app/src/main/java/com/example/dangle_lotto/ui/create_event/CreateEventFragment.java
+++ app/src/main/java/com/example/dangle_lotto/ui/create_event/CreateEventFragment.java
@@ -1,4 +1,12 @@
 package com.example.dangle_lotto.ui.create_event;
 
+/**
+ * CreateEventFragment - This fragment will be displayed whenever a GeneralUser
+ * wants to organize an event.
+ *
+ * @author Annie Ding
+ * @version 1.0
+ * @since 2025-11-05
+ */
 public class CreateEventFragment {
 }

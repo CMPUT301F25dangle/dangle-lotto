6d3a876ca4e814ee0bdedad469edb30d9a3da750
Mahd Afzal <afzalmahd@gmail.com>
2025-11-05 23:34:46 -0700

    some testing stuff

--- app/src/test/java/com/example/dangle_lotto/EventUnitTest.java
+++ app/src/test/java/com/example/dangle_lotto/EventUnitTest.java
@@ -3,6 +3,12 @@ package com.example.dangle_lotto;
 import org.junit.Test;
 
 public class EventUnitTest {
+    private final FirebaseManager firebaseManager;
+
+    public EventUnitTest() {
+        firebaseManager = new FirebaseManager(true);
+    }
+
     @Test
     public void testEventCreation() {
     }

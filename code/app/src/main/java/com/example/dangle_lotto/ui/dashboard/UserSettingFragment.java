c8ee30e9aec8153a34f6343860f6587aca3d6236
amongus <adi4soni@gmail.com>
2025-11-23 04:33:22 -0700

    lots of tests, trying to figure our pageination for your events, but its very weird

--- code/app/src/main/java/com/example/dangle_lotto/ui/dashboard/UserSettingFragment.java
+++ code/app/src/main/java/com/example/dangle_lotto/ui/dashboard/UserSettingFragment.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.dangle_lotto.FirebaseManager;
@@ -16,6 +19,7 @@ import com.example.dangle_lotto.GeneralUser;
 import com.example.dangle_lotto.LoginActivity;
 import com.example.dangle_lotto.UserViewModel;
 import com.example.dangle_lotto.databinding.FragmentUserSettingBinding;
+import com.example.dangle_lotto.ui.login.SimpleTextWatcher;
 
 /**
  * UserSettingFragment - Fragment shows user settings.
@@ -29,7 +33,11 @@ public class UserSettingFragment extends Fragment {
     private UserViewModel userViewModel;
     private FirebaseManager firebaseManager;
     private GeneralUser user;
-    private boolean confirm = false;
+    private boolean confirmDelete = false;
+    private boolean confirmUpdate = false;
+    private EditText usernameEditText;
+    private EditText emailEditText;
+    private EditText phoneEditText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -53,16 +61,11 @@ public class UserSettingFragment extends Fragment {
             requireActivity().getSupportFragmentManager().popBackStack();
         });
 
-        // setting edit text views with user info
-        binding.settingsFragmentUsername.setText(user.getName());
-        binding.settingsFragmentEmail.setText(user.getEmail());
-        binding.settingsFragmentPhoneNumber.setText(user.getPhone());
-
         // making delete button delete the user
-        binding.button.setOnClickListener(v -> {
-            if (!confirm) {
-                binding.button.setText("Confirm Delete");
-                confirm = true;
+        binding.userSettingsDeleteButton.setOnClickListener(v -> {
+            if (!confirmDelete) {
+                binding.userSettingsDeleteButton.setText("Confirm Delete");
+                confirmDelete = true;
             } else {
                 user.delete();
                 Toast.makeText(getActivity(), "User deleted", Toast.LENGTH_SHORT).show();
@@ -73,6 +76,92 @@ public class UserSettingFragment extends Fragment {
             }
         });
 
+        // attaching edit text views
+        usernameEditText = binding.settingsFragmentUsername;
+        emailEditText = binding.settingsFragmentEmail;
+        phoneEditText = binding.settingsFragmentPhoneNumber;
+
+        // setting edit text views with user info
+        usernameEditText.setText(user.getName());
+        emailEditText.setText(user.getEmail());
+        phoneEditText.setText(user.getPhone());
+
+        // updating button state
+        updateButtonState();
+
+        // setting up text watcher
+        TextWatcher watcher = new SimpleTextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                updateButtonState();
+            }
+        };
+        usernameEditText.addTextChangedListener(watcher);
+        emailEditText.addTextChangedListener(watcher);
+        phoneEditText.addTextChangedListener(watcher);
+
+        // clicking button to update user info
+        binding.userSettingsUpdateButton.setOnClickListener(v -> {
+            String username = usernameEditText.getText().toString().trim();
+            String email = emailEditText.getText().toString().trim();
+            String phone = phoneEditText.getText().toString().trim();
+
+            if (TextUtils.isEmpty(username)) {
+                usernameEditText.setError("Name required");
+                return;
+            }
+
+            if (TextUtils.isEmpty(email)) {
+                emailEditText.setError("Email required");
+                return;
+            }
+
+            // Need to hit the button twice (confirm system used)
+            if (!confirmUpdate) {
+                binding.userSettingsUpdateButton.setText("Confirm Update");
+                confirmUpdate = true;
+            } else {
+                GeneralUser newUser = new GeneralUser(
+                        user.getUid(),
+                        username,
+                        email,
+                        phone,
+                        user.getPhotoID(),
+                        firebaseManager,
+                        user.canOrganize()
+                );
+
+                // updating user info
+                user = newUser;
+                firebaseManager.updateUser(newUser);
+                userViewModel.setUser(newUser);
+
+                // unselecting edit text fields
+                usernameEditText.clearFocus();
+                emailEditText.clearFocus();
+                phoneEditText.clearFocus();
+
+                // updating button state
+                updateButtonState();
+                binding.userSettingsUpdateButton.setText("Update Profile");
+            }
+        });
+
         return root;
+    }
+
+    /**
+     * Updates the button state based on the current user information.
+     */
+    private void updateButtonState() {
+        String username = usernameEditText.getText().toString().trim();
+        String email = emailEditText.getText().toString().trim();
+        String phone = phoneEditText.getText().toString().trim();
+
+        String originalPhone = user.getPhone() == null ? "" : user.getPhone();
+
+        boolean same = username.equals(user.getName()) && email.equals(user.getEmail()) && phone.equals(originalPhone);
+
+        binding.userSettingsUpdateButton.setEnabled(!same);
     }
 }

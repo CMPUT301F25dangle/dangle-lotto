fc784afb2088a20f4dc03913e3f5b4ae95ec6758
Annie D <ading2@ualberta.ca>
2025-11-29 16:49:07 -0700

    pics

--- code/app/src/main/java/com/example/dangle_lotto/ui/admin/AdminViewImagesFragment.java
+++ code/app/src/main/java/com/example/dangle_lotto/ui/admin/AdminViewImagesFragment.java
@@ -6,12 +6,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.dangle_lotto.Event;
 import com.example.dangle_lotto.FirebaseCallback;
 import com.example.dangle_lotto.FirebaseManager;
 import com.example.dangle_lotto.databinding.FragmentAdminViewImagesBinding;
@@ -19,6 +23,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.Query;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * AdminViewImagesFragment - This fragment displays all images
@@ -35,8 +40,10 @@ public class AdminViewImagesFragment extends Fragment {
     private final FirebaseManager firebaseManager = FirebaseManager.getInstance();
     private FragmentAdminViewImagesBinding binding;
     private ArrayList<String> images;
+    private ArrayList<Event> events;
     private RecyclerView recyclerView;
     private ImageCardAdapter adapter;
+    AdminViewModel adminViewModel;
     private LinearLayoutManager manager;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -46,42 +53,82 @@ public class AdminViewImagesFragment extends Fragment {
         manager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(manager);
         images = new ArrayList<>();
-        adapter = new ImageCardAdapter(images, position -> {
-            Query query = firebaseManager.getEventsReference().whereNotEqualTo("Picture", null);
-            firebaseManager.getQuery(null, query, new FirebaseCallback<ArrayList<DocumentSnapshot>>() {
-                @Override
-                public void onSuccess(ArrayList<DocumentSnapshot> result) {
-                    int startPos = images.size();
-                    for (DocumentSnapshot doc : result) {
-                        images.add(firebaseManager.documentToEvent(doc).getPhotoID());
-                    }
-                    adapter.notifyItemRangeInserted(startPos, images.size());
-                }
+        events = new ArrayList<>();
+        adminViewModel = new ViewModelProvider(this).get(AdminViewModel.class);
 
-                @Override
-                public void onFailure(Exception e) {
-                    Log.d("Firebase", "Failed to load iamges", e);
-                }
+        // Setup an adapter and attach it to the recycler view
+        adapter = new ImageCardAdapter(images, position -> {
+            String image = images.get(position);
+
+            // Show dialog to confirm action
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+            // Title
+            builder.setTitle("Remove image");
+            builder.setMessage("Are you sure you want to remove this image?");
+
+            builder.setPositiveButton("Delete", (dialogInterface, i) -> {
+                Toast.makeText(getContext(), "Removed image", Toast.LENGTH_SHORT).show();
+                // TODO: Notify user
+                firebaseManager.deletePic(image, new FirebaseCallback<Void>() {
+                    @Override
+                    public void onSuccess(Void result) {
+                        // Remove image from list
+                        images.remove(image);
+                        adapter.notifyItemRemoved(i);
+                        adapter.notifyItemRangeChanged(i, images.size() - i);
+                    }
+
+                    @Override
+                    public void onFailure(Exception e) {
+                        Log.e("Firebase", "failed to delete image: " + image, e);
+                    }
+                });
             });
+            builder.setNegativeButton("Cancel", (dialogInterface, i) -> dialogInterface.dismiss());
+            builder.show();
+
         });
         recyclerView.setAdapter(adapter);
-        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-                // only check when scrolling down
-                if (dy <= 0) return;
 
-                int visibleItemCount = manager.getChildCount();
-                int totalItemCount = manager.getItemCount();
-                int firstVisibleItemPosition = manager.findFirstVisibleItemPosition();
+        firebaseManager.getAllEvents(new FirebaseCallback<ArrayList<String>>() {
+            @Override
+            public void onSuccess(ArrayList<String> result) {
+                events.clear();
+                images.clear();
+
+                for (String id : result) {
+                    firebaseManager.getEvent(id, new FirebaseCallback<Event>() {
+                        @Override
+                        public void onSuccess(Event event) {
+                            images.add(event.getPhotoID());
+                            events.add(event);
+                            adapter.notifyDataSetChanged();
+
+                        }
+
+                        @Override
+                        public void onFailure(Exception e) {
+                            Log.e("Firebase", "failed to fetch event: " + id, e);
+                        }
+                    });
+                }
+                if (result.isEmpty()) {
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                Log.e("Firebase", "failed to fetch all events", e);
             }
         });
         return root;
     }
+
     @Override
-    public void onDestroyView(){
+    public void onDestroyView() {
         super.onDestroyView();
-        binding= null;
+        binding = null;
     }
 }

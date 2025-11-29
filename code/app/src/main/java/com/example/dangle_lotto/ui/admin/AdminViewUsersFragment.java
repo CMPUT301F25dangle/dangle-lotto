366ebba0ca6c6eb227f5ab9d7e68135c1690c478
amongus <adi4soni@gmail.com>
2025-11-29 15:46:15 -0700

    fixed deleting users and all that

--- code/app/src/main/java/com/example/dangle_lotto/ui/admin/AdminViewUsersFragment.java
+++ code/app/src/main/java/com/example/dangle_lotto/ui/admin/AdminViewUsersFragment.java
@@ -1,10 +1,13 @@
 package com.example.dangle_lotto.ui.admin;
 
+import android.app.AlertDialog;
+import android.health.connect.GetMedicalDataSourcesRequest;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -13,10 +16,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.dangle_lotto.FirebaseCallback;
 import com.example.dangle_lotto.FirebaseManager;
+import com.example.dangle_lotto.GeneralUser;
 import com.example.dangle_lotto.User;
 import com.example.dangle_lotto.databinding.FragmentAdminViewUsersBinding;
 
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -32,46 +37,59 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class AdminViewUsersFragment extends Fragment {
     private FragmentAdminViewUsersBinding binding;
     private final FirebaseManager firebaseManager = FirebaseManager.getInstance();
-    public ArrayList<User> users;
+    public ArrayList<GeneralUser> users;
     private RecyclerView recyclerView;
     private UserCardAdapter adapter;
     private LinearLayoutManager manager;
-    private User selectedUser;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceBundle) {
+        // Inflate the layout for this fragment
         binding = FragmentAdminViewUsersBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        // Set up recycler view
         recyclerView = binding.adminUsersList;
         manager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(manager);
         users = new ArrayList<>();
-        adapter = new UserCardAdapter(users, position -> {
-            firebaseManager.getUser(users.get(position).getUid(), new FirebaseCallback<User>() {
-                @Override
-                public void onSuccess(User result) {
-                    selectedUser = result;
-                }
 
-                @Override
-                public void onFailure(Exception e) {
-                    Log.e("Firebase", "failed to fetch selected user", e);
-                }
+        // Set up adapter and attach it to the recycler view
+        adapter = new UserCardAdapter(users, position -> {
+            GeneralUser user = users.get(position);
+
+            // Show dialog to confirm action
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+            // Title
+            builder.setTitle("Manage " + user.getName());
+
+            // Organizer button
+            if (user.canOrganize()) {
+                builder.setPositiveButton("Remove organizer", (dialogInterface, i) -> {
+                    Toast.makeText(getContext(), "Removed organization rights from" + user.getName(), Toast.LENGTH_SHORT).show();
+                    // TODO: Notify user
+
+                    // Remove organizer privilege
+                    user.setCanNotOrganize();
+                });
+            }
+
+            // User button button
+            builder.setNegativeButton("Delete user ", (dialogInterface, i) -> {
+                Toast.makeText(getContext(), "Removed user " + user.getName(), Toast.LENGTH_SHORT).show();
+                firebaseManager.deleteUser(user.getUid());
+
+                // Remove user from list
+                users.remove(user);
+                adapter.notifyItemRemoved(i);
+                adapter.notifyItemRangeChanged(i, users.size() - i);
             });
+
+            // Cancel button
+            builder.setNeutralButton("Cancel", (dialogInterface, i) -> dialogInterface.dismiss());
+            builder.show();
         });
         recyclerView.setAdapter(adapter);
-
-        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-                // only check when scrolling down
-                if (dy <= 0) return;
-
-                int visibleItemCount = manager.getChildCount();
-                int totalItemCount = manager.getItemCount();
-                int firstVisibleItemPosition = manager.findFirstVisibleItemPosition();
-            }
-        });
 
         firebaseManager.getAllUsers(new FirebaseCallback<ArrayList<String>>() {
             @Override
@@ -88,7 +106,7 @@ public class AdminViewUsersFragment extends Fragment {
                         @Override
                         public void onSuccess(User user) {
                             if (!user.isAdmin()) {
-                                users.add(user);
+                                users.add((GeneralUser) user);
                                 adapter.notifyDataSetChanged();
                             }
                         }
--- code/app/src/main/java/com/example/dangle_lotto/ui/admin/UserCardAdapter.java
+++ code/app/src/main/java/com/example/dangle_lotto/ui/admin/UserCardAdapter.java
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.dangle_lotto.FirebaseManager;
+import com.example.dangle_lotto.GeneralUser;
 import com.example.dangle_lotto.R;
 import com.example.dangle_lotto.User;
 import com.example.dangle_lotto.ui.EventCardAdapter;
@@ -33,7 +34,7 @@ import javax.annotation.Nonnull;
  * @since 2025-11-27
  */
 public class UserCardAdapter extends RecyclerView.Adapter<UserCardAdapter.ViewHolder> {
-    private static ArrayList<User> users = new ArrayList<>();
+    private static ArrayList<GeneralUser> users = new ArrayList<>();
     private final OnItemClickListener listener;
 
     private static final FirebaseManager firebaseManager = FirebaseManager.getInstance();
@@ -41,7 +42,7 @@ public class UserCardAdapter extends RecyclerView.Adapter<UserCardAdapter.ViewHo
         void onItemClick(int position);
     }
 
-    public UserCardAdapter(ArrayList<User> Users, OnItemClickListener listener) {
+    public UserCardAdapter(ArrayList<GeneralUser> Users, OnItemClickListener listener) {
         users = Users;
         this.listener = listener;
     }
@@ -82,29 +83,16 @@ public class UserCardAdapter extends RecyclerView.Adapter<UserCardAdapter.ViewHo
         public void bind(User user, OnItemClickListener listener) {
             name.setText(user.getName());
             email.setText(user.getEmail());
+
+            // load default image if no image is provided
             if (user.getPhotoID() == null || user.getPhotoID().isEmpty()) {
                 imageView.setImageResource(R.drawable.default_profile_picture);
             } else {
                 Glide.with(imageView.getContext()).load(user.getPhotoID()).into(imageView);
             }
-            itemView.setOnClickListener(v -> {
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
-                builder.setTitle("Manage " + user.getName());
-                builder.setPositiveButton("Remove organizer", (dialogInterface, i) -> {
-                    Toast.makeText(v.getContext(), "Removed organization rights from" + user.getName(), Toast.LENGTH_SHORT).show();
-                    // TODO: Notify user
-                    firebaseManager.revokeOrganizer(user.getUid());
-                    listener.onItemClick(getBindingAdapterPosition());
-                });
-                builder.setNegativeButton("Delete user ", (dialogInterface, i) -> {
-                    Toast.makeText(v.getContext(), "Removed user " + user.getName(), Toast.LENGTH_SHORT).show();
-                    firebaseManager.deleteUser(user.getUid());
-                });
-                builder.setNeutralButton("Cancel", (dialogInterface, i) -> dialogInterface.dismiss());
-                builder.show();
-            });
+            // Set up click listener for the item
+            itemView.setOnClickListener(v -> listener.onItemClick(getBindingAdapterPosition()));
         }
-
     }
 }
